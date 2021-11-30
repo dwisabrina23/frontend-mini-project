2ffaecad0fb2f6447b1ad8500af67c1974ee66f7
dwisabrina23 <dwisabrina10@gmail.com>
2021-11-30 21:29:23 +0800

    data: add static data

---
+++ src/Models/ServiceData.js
@@ -0,0 +1,23 @@
+import image1 from "../assets/service-img1.png"
+import image2 from "../assets/service-img3.png"
+import image3 from "../assets/service-img2.png"
+
+const ServiceData = [
+    {
+        photo:image1,
+        title:"INTERIOR PAINTING",
+        content:"whether an existing home or a new build, the interior paint is an important and having dun finishing touch"
+    },
+    {
+        photo:image2,
+        title:"EXTERIOR PAINTING",
+        content:"upgrade your curb appeal, keep your space looking new and pretty"
+    },
+    {
+        photo:image3,
+        title:"COMMERCIAL",
+        content:"keep your employes and custommers comming back with a vibrant, freshly-painted, commercial space"
+    },
+]
+
+export default ServiceData
